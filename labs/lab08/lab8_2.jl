36f2fdf41e9da6b7754e73e9e82dbfde7b483d87
Victoria <74490985+lumotori@users.noreply.github.com>
2023-04-01 23:00:50 +0300

    Add files via upload

---
+++ labs/lab08/lab8_2.jl
@@ -0,0 +1,52 @@
+using DifferentialEquations
+using Plots
+
+p_cr = 19
+N = 17.5
+q = 1
+tau1 = 12
+tau2 = 16
+p1 = 10
+p2 = 6.6
+
+
+a1 = p_cr/(tau1*tau1*p1*p1*N*q)
+a2 = p_cr/(tau2*tau2*p2*p2*N*q)
+b = p_cr/(tau1*tau1*tau2*tau2*p1*p1*p2*p2*N*q) 
+c1 = (p_cr-p1)/(tau1*p1)
+c2 = (p_cr-p2)/(tau2*p2)
+
+function Fun(du, u, p, t)
+    M1, M2 = u
+    du[1] = u[1]-(b/c1+0.00019)*u[1]*u[2]-a1/c1*u[1]*u[1]
+    du[2] = c2/c1*u[2]-b/c1*u[1]*u[2]-a2/c1*u[2]*u[2]
+end
+
+v = [2.6, 1.9]
+time = (0.0, 30.0)
+prob = ODEProblem(Fun, v, time)
+sol = solve(prob, dtmax = 0.05)
+M1 = [u[1] for u in sol.u]
+M2 = [u[2] for u in sol.u]
+T = [t for t in sol.t]
+
+plt = plot(
+    dpi = 300,
+    legend =:topright)
+
+plot!(
+    plt,
+    T,
+    M1,
+    label = "M1",
+    color = :red)
+
+plot!(
+    plt,
+    T,
+    M2,
+    label = "M2",
+    color = :blue)
+
+
+
